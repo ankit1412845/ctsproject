e845cd1d58176a7a8dfe33cd828e85d87178e67d
Ankit <soumajitx@gmail.com>
2026-05-09 19:10:06 +0530

    update project

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import login, home, semester
+from .views import login, home, semester, civil, electrical, mechanical
 
 urlpatterns = [
     path('', login, name='login'),
     path('home/', home, name='home'),
     path('semester/', semester, name='semester'),
+    path('civil/', civil, name='civil'),
+    path('electrical/', electrical, name='electrical'), 
+    path('mechanical/', mechanical, name='mechanical'),
+
 ]
