f76917d160e04f29bbd80b57360a5008e4235b69
Ankit <soumajitx@gmail.com>
2026-05-09 18:53:59 +0530

    update project

--- main/views.py
+++ main/views.py
@@ -8,3 +8,12 @@ def home(request):
 
 def semester(request):
     return render(request, 'main/semester.html')
+
+def civil(request):
+    return render(request, 'main/civil.html')
+
+def electrical(request):
+    return render(request, 'main/electrical.html')
+
+def mechanical(request):
+    return render(request, 'main/mechanical.html')
