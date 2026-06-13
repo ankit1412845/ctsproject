f8baefa1bbb51009d80280f14d58b63d79a39988
Ankit <soumajitx@gmail.com>
2026-06-13 22:41:59 +0530

    Fix department wise semester page

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login_page, name='login'),
     path('home/', views.home, name='home'),
-    path('semester/', views.semester, name='semester'),
+    path('semester/<str:dept_name>/', views.semester, name='semester'),
 
     path('upload/<int:sem_no>/', views.upload_pdf, name='upload_pdf'),
 
