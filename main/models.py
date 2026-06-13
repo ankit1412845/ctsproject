9bc494c70cbec273808152b3439c7e1bd5483ca9
Ankit <soumajitx@gmail.com>
2026-06-13 22:04:00 +0530

    Fix SubjectPDF model indentation

--- main/models.py
+++ main/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class PDFUpload(models.Model):
     semester = models.IntegerField()
     pdf_file = models.FileField(upload_to='pdfs/')
 
     def __str__(self):
         return self.pdf_file.name
-    
-    class SubjectPDF(models.Model):
+
+
+class SubjectPDF(models.Model):
     department = models.CharField(max_length=100)
     semester = models.IntegerField()
     subject = models.CharField(max_length=150)
